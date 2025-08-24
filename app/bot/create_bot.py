c8a8808028b32f19760f19c1358af8e5f15755cb
flam7060 <kripa733@ya.ru>
2025-08-24 20:52:42 +0300

    feat: update megalinter

--- app/bot/create_bot.py
+++ app/bot/create_bot.py
@@ -1,12 +1,12 @@
-from aiogram import Dispatcher, Bot
-from app.bot.handlers import router_horoscop, router_get_cat, router_get_dog
+from aiogram import Bot, Dispatcher
+from app.bot.handlers import router_get_cat, router_get_dog, router_horoscop
 
 
 async def create_bot(bot_token: str):
     bot = Bot(token=bot_token)
     dp = Dispatcher()
     dp.include_routers(router_horoscop, router_get_dog, router_get_cat)
-    print("Bot worked", flush=1)
+    print("Bot worked", flush=True)
     try:
         await dp.start_polling(bot)
     finally:
